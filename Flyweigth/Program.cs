46029991e00b45acf83e9155b03eba201628e049
Hudson da Silva Carlos <hudsonscarlos@outlook.com>
2021-08-15 21:59:13 -0300

    Criando todos os projetos.

---
+++ Flyweigth/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Flyweigth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
